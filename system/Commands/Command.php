ec586f90aadd241d773e25aae767643110c3a41e
rajakodings <rajakodings@gmail.com>
2019-10-24 12:13:20 +0700

    Update

--- system/Commands/Command.php
+++ system/Commands/Command.php
@@ -13,7 +13,7 @@ class Command
     public function makeClassName($class_file) {
         $class_name = str_replace(base_path(),"",$class_file);
         $class_name = str_replace("/","\\", $class_name);
-        $class_name = str_replace("\app","\App", $class_name);
+        $class_name = str_replace("app\\","App\\", $class_name);
         $class_name = str_replace(".php","", $class_name);
         return $class_name;
     }
