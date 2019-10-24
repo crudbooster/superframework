7060d679d73b881d3a7f3015312c83cc1b4aecc7
rajakodings <rajakodings@gmail.com>
2019-10-24 12:11:39 +0700

    Update

--- system/Commands/Command.php
+++ system/Commands/Command.php
@@ -11,7 +11,7 @@ class Command
      * @return mixed|string
      */
     public function makeClassName($class_file) {
-        $class_name = str_replace(getcwd(),"",$class_file);
+        $class_name = str_replace(base_path(),"",$class_file);
         $class_name = str_replace("/","\\", $class_name);
         $class_name = str_replace("\app","\App", $class_name);
         $class_name = str_replace(".php","", $class_name);
