a879aacaeb5a09ba2ccaecfb2c72c4950517702e
rajakodings <rajakodings@gmail.com>
2020-01-25 10:02:25 +0700

    Update

--- app/Configs/routes.php
+++ app/Configs/routes.php
@@ -7,8 +7,8 @@ return [
     'admin/logout' => 'App\\Modules\\Admin\\Controllers\\AdminController@logout',
     'admin-api/auth' => 'App\\Modules\\AdminAPI\\Controllers\\ApiAuthController@index',
     'admin-api/auth/check-session' => 'App\\Modules\\AdminAPI\\Controllers\\ApiAuthController@checkSession',
-    'admin-api/users' => 'App\\Modules\\AdminAPI\\Controllers\\ApiUsersController@index',
-    'admin-api/users/update-profile' => 'App\\Modules\\AdminAPI\\Controllers\\ApiUsersController@updateProfile',
+    'admin-api/profile' => 'App\\Modules\\AdminAPI\\Controllers\\ApiProfileController@index',
+    'admin-api/profile/update' => 'App\\Modules\\AdminAPI\\Controllers\\ApiProfileController@update',
     'home' => 'App\\Modules\\Site\\Controllers\\Home@index',
     'home/index' => 'App\\Modules\\Site\\Controllers\\Home@index',
 ];
